8cfbd12f16a0efcf8ab26dcd07ca00f70ccab8f7
SaiMahith123 <113820807+SaiMahith123@users.noreply.github.com>
2026-03-05 19:08:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,15 @@ if "GROQ_API_KEY" in st.secrets:
 else:
     st.error("Missing API Key! Please add GROQ_API_KEY in Streamlit Secrets.")
     st.stop()
-  if "messages" not in st.session_state:
+if "messages" not in st.session_state:
     st.session_state.messages = []
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 if prompt := st.chat_input("Ask me anything..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-
-    with st.chat_message("assistant"):
-        with st.spinner("Processing..."):
-            response = llm.invoke(st.session_state.messages)
-            st.write(response.content)
-            st.session_state.messages.append({"role": "assistant", "content": response.content})
+with st.chat_message("assistant"):
+    with st.spinner("Processing..."):
+        response = llm.invoke(st.session_state.messages)
+        st.write(response.content)
+        st.session_state.messages.append({"role": "assistant", "content": response.content})
